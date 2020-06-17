da33f7520f11ea55be63ba91a88fa2bafac1420b
David M. Weigl <weigl@mdw.ac.at>
2020-06-17 17:12:32 +0200

    Initial commit of public materials

---
+++ static/viewer-public.js
@@ -0,0 +1,165 @@
+// websocket variable (initialized in $(document).ready
+var socket;
+
+var strata = ["Content", "Processing", "Engineering", "Interface", "Cognitive", "Affective", "Situational", "Social Context", "Cultural Context"];
+var heatCellWidth = 82;
+var heatCellHeight = 83;
+var heatmap, ctx, parentOffset, fudgeX, fudgeY; // to be initialised in $(document).ready
+var img = new Image();
+
+function heatmapRelativePosition(x, y)  { 
+        // position 0,0 at the top-left corner of the top-left heatmap cell:
+        x -= fudgeX;
+        y -= fudgeY;
+        cellx = Math.floor(x/heatCellWidth);
+        celly = Math.floor(y/heatCellHeight);
+        return {"x":cellx, "y": celly};
+
+}
+
+function displayStratComps(stratComps) { 
+    var stratCompsHTML = ""
+    for (sCTuple in stratComps) { 
+        var tupleID = stratComps[sCTuple][1]["tupleID"].join("_");
+        var freq = stratComps[sCTuple][1]["freq"];
+        var numStudies = stratComps[sCTuple][1]["numStudies"];
+        var findings = stratComps[sCTuple][1]["findings"].join("_");
+        var sC = stratComps[sCTuple][1]["stratComps"];
+
+        // sort sC's according to stratum
+        sC = sC.sort(function(a, b) { 
+            return strata.indexOf(a["stratum"]) - strata.indexOf(b["stratum"]);
+        })
+
+        sCTuple = ""
+        for (s in sC) { 
+            sCTuple += '<span class="stratCompDescriptor" title="' + sC[s]["hint"] + 
+                        '">' + sC[s]["stratum"] + ": " + sC[s]["description"] + "</span>";
+        }
+        
+        stratCompsHTML +=
+            '<div class="stratCompTuple" id="' + tupleID + '" data-findings="' + findings + '" onclick="showFindings(event, this)"><span class="stratCompTupleFreq">' + numStudies + " studies, " + freq + " findings: </span>" + sCTuple + "<span class='expand'>+</span><div class='findings'></div></div>";
+    }
+    $("#stratComps").html(stratCompsHTML);
+}
+
+function showFindings(e, element) { 
+    var stateIndicator = $(element).children(".expand");
+    if ($(stateIndicator).hasClass("closed")) { 
+        // user wants to reopen the previously closed div
+        $(stateIndicator).removeClass("closed");
+        $(stateIndicator).addClass("open");
+        $(stateIndicator).html("-");
+        $(element).children(".findings").css("display", "block");
+    }
+    else if($(stateIndicator).hasClass("open")) {
+        // user wants to hide the findings for this stratCompTuple 
+        $(stateIndicator).html("+");
+        $(stateIndicator).removeClass("open");
+        $(stateIndicator).addClass("closed");
+        $(element).children(".findings").css("display", "none");
+    }
+    else { 
+        // we haven't loaded the findings yet, so request them and open the div:
+        socket.emit('showFindingsRequest', {
+            "tupleID": $(element).attr("id"),
+            "findings": $(element).attr("data-findings").split("_")
+        });
+        $(stateIndicator).html("-");
+        $(stateIndicator).addClass("open");
+    }
+}
+
+
+$(document).ready(function() { 
+    // set up websocket
+    socket = io.connect('http://' + document.domain + ':' + location.port);
+    socket.on('connect', function() { 
+        socket.emit('clientConnectionEvent', 'Client connected.');
+        console.log('Connected to server.');
+    });
+    socket.on('stratCompRequestHandled', function(sC) { 
+        $("#loading").html("");
+        displayStratComps(sC);
+    });
+
+    socket.on('showFindingsRequestHandled', function(message) { 
+        var findingsHTML = "";
+        for (var f in message["findings"]) { 
+            findingsHTML += 
+            "<div class='finding'>" + 
+            "<div class='findingid'><span class='header'>FindingID:</span> " + message["findings"][f]["findingid"] + "</div>" +
+            "<div class='fdescription'><span class='header'>Description:</span> " + message["findings"][f]["f.description"] +"</div>" +
+                    "<div class='sid'><span class='header'>StudyID:</span> " + message["findings"][f]["s.id"] + "</div>" +
+                    "<div class='sdescription'><span class='header'>Study description:</span> " + message["findings"][f]["s.description"]+ "</div>" +
+                    "<div class='smethodology'><span class='header'>Study methodology:</span> " + message["findings"][f]["s.methodology"] +"</div>" +
+                    "<div class='ssampleframe'><span class='header'>Study sample frame:</span> " + message["findings"][f]["s.sampleframe"] +"</div>" +
+                    "<div class='ssamplesize'><span class='header'>Study sample size:</span> " + message["findings"][f]["s.samplesize"] +"</div>" +
+                  //  "<div class='slimitations'><span class='header'>Study limitations:</span> " + message["findings"][f]["s.limitations"] +"</div>"+ 
+                   // "<div class='scomments'><span class='header'>Study comments:</span> " + message["findings"][f]["s.comments"] + "</div>" +
+                   // "<div class='sreliability'><span class='header'>Study reliability:</span> " + message["findings"][f]["s.reliability"]+ "</div>"+
+                    "<div class='atitle'><span class='header'>Article title:</span> " + message["findings"][f]["a.title"]+ "</div>"+
+                    "<div class='aauthors'><span class='header'>Article authors:</span> " + message["findings"][f]["a.authors"]+ "</div>"+
+                    "<div class='ayear'><span class='header'>Year of publication:</span> " + message["findings"][f]["a.year"]+ "</div>"+
+                    "<div class='ajournal'><span class='header'>Article journal:</span> " + message["findings"][f]["a.journal"]+ "</div>"+
+                    "<div class='aid'><span class='header'>Article ID:</span> " + message["findings"][f]["a.id"]+ "</div>"+
+                    "<div class='scholar'><span class='header'>Scholar:</span>" + 
+                    "<span class='scholarlink'>" + 
+                    "<a href='http://scholar.google.ca/scholar?q="  + 
+                        encodeURI(message["findings"][f]["a.title"])  + 
+                        "' target='_blank'>Search for article</a></span></div>" + 
+                    "</div>";
+                }
+        $('#' + message["tupleID"]).children(".findings").html(findingsHTML);
+        $('.scholarlink').click(function(e) { e.stopPropagation() });
+    });
+        
+    heatmap = document.getElementById("heatmap");
+    ctx = heatmap.getContext("2d");
+    parentOffset = $("#heatmap").parent().offset();
+    fudgeX = 109 + parentOffset.left;
+    fudgeY = parentOffset.top;
+
+    img.addEventListener("load", function() { 
+        ctx.drawImage(img, 0, 0);
+    }, false);
+    img.src = "static/heatmap.png";
+    
+
+    $("#heatmap").click(function(e) { 
+        var cellPos = heatmapRelativePosition(e.pageX, e.pageY)
+        var cellx = cellPos["x"];
+        var celly = cellPos["y"];
+
+        if(cellx >= 0 && cellx < strata.length && celly >= 0 && celly < strata.length) {
+            $("#selected_left").html(strata[cellx]);
+            $("#selected_right").html(strata[strata.length-1-celly]);
+            
+            $("#loading").html(">> Loading stratum component descriptors list...");
+	    $("#stratComps").html("");
+            socket.emit('stratCompRequest',[strata[cellx], strata[strata.length-1-celly]] );
+
+        }
+
+    })
+    
+    $("#heatmap").mousemove(function(e) { 
+        // refresh the image
+        ctx.drawImage(img, 0, 0);
+        // draw a highlighting rectangle around the appropriate cell
+        var cellPos = heatmapRelativePosition(e.pageX, e.pageY)
+        var cellx = cellPos["x"];
+        var celly = cellPos["y"];
+
+        if(cellx >= 0 && cellx < strata.length && celly >= 0 && celly < strata.length) {
+            // we are on the grid
+            ctx.beginPath();
+            ctx.fillStyle="rgba(225, 150,150, 0.5";
+            ctx.fillRect(fudgeX + (cellx*heatCellWidth), fudgeY + (celly*heatCellHeight), heatCellWidth, heatCellHeight);
+            ctx.strokeStyle="brown";
+            ctx.rect(fudgeX + (cellx*heatCellWidth), fudgeY + (celly*heatCellHeight), heatCellWidth, heatCellHeight);
+            ctx.stroke();
+            //console.log("Parent offsets: ", parentOffset.left, ", ",parentOffset.top, " Mouse at: page - ", e.pageX, ",", e.pageY,", canvas: ", x, ",", y,", cellx: ", cellx, "celly: ", celly, "on labels: ", strata[cellx], ", ",strata[strata.length-1-celly]);
+        }
+    })
+})
---
+++ viewer-public.py
@@ -0,0 +1,242 @@
+from flask import Flask, render_template, request
+#from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
+from flask_socketio import SocketIO, emit
+import json
+import operator
+import sqlite3
+import os
+from pprint import pprint
+
+app = Flask(__name__)
+app.debug = False
+app.config["SECRET_KEY"] = os.environ.get("MIRELEVANCESECRET") or 'asdfasdgafsgfadwsxcbget4r3qwasdft'
+app.config["SQLITEDB"] = os.environ.get("MIRELEVANCEDB") or "relevance-public.db"
+
+socketio = SocketIO(app)
+
+db = sqlite3.connect(app.config["SQLITEDB"])
+
+
+@app.route('/')
+def index():
+    return render_template("index-public.html")
+
+
+@app.route('/heatmap')
+def heatmap():
+        return render_template("index-public.html") 
+
+@app.route('/query')
+def query():
+        response = app.make_response(render_template("query.html"))
+        return response
+
+def extractStratCompIds(tup):
+    return tup[0].replace("stratComp", "")
+
+@app.route('/queryFindings')
+def queryFindings():
+    tupleID = request.args["tupleID"]
+    print(("Got :", tupleID))
+    findingIDs = request.args["findings"].split("_")
+    print("Findings :")
+    pprint(findingIDs)
+    c = db.cursor()
+    findingsDetailsQuery = """
+        select f.id, f.description, s.id, s.description, s.methodology, s.sampleframe, s.samplesize, s.limitations, s.comments, s.reliability, a.title, a.authors, a.journal, a.year, a.id
+        from findings f 
+        join studies s on f.studyid = s.id
+        join articles a on s.articleid = a.id
+        WHERE f.id IN ("""
+    # add as many placeholders for the "WHERE f.id IN" clause as there are finding ids
+    findingsDetailsQuery += ",".join(["?"]*len(findingIDs)) + ");"
+    findings = list()
+    for row in c.execute(findingsDetailsQuery, findingIDs):
+        findings.append({ 
+            "findingid": row[0],
+            "f.description": row[1],
+            "s.id": row[2],
+            "s.description": row[3],
+            "s.methodology": row[4],
+            "s.samplesize": row[5],
+            "s.sampleframe": row[6],
+          # "s.limitations": row[7],
+          # "s.comments": row[8],
+          # "s.reliability": row[9],
+            "a.title": row[10],
+            "a.authors": row[11],
+            "a.journal": row[12],
+            "a.year": row[13],
+	    "a.id": row[14]})
+    return json.dumps({
+        "tupleID": tupleID,
+        "findings": findings
+    });
+
+@app.route('/querySubmit')
+def querySubmit():
+    findings = list();
+    findingStratComps = dict();
+    c = db.cursor()
+    stratCompIds = list(map(extractStratCompIds, list(request.args.items())))
+    findingsByStratCompsQuery = ""
+    for ix, scId in enumerate(stratCompIds):
+        findingsByStratCompsQuery += "SELECT fi.findingid FROM findingInstances fi WHERE fi.stratCompId = ?"
+        if ix < len(stratCompIds)-1:
+            findingsByStratCompsQuery += " INTERSECT "
+    for row in c.execute(findingsByStratCompsQuery, stratCompIds):
+        findings.append(row[0])
+    print(findings)
+    # now build the query that fetches us strat descriptors for all the findings we've retrieved
+    stratDescQuery = """
+        select f.id, f.studyid, sc.id, sc.stratum, sc.description, sc.hint
+        from findings f 
+        join findingInstances fi on f.id = fi.findingid
+        join stratumComponents sc on fi.stratcompid = sc.id
+        WHERE f.id IN ("""
+    # add as many placeholders for the "WHERE f.id IN" clause as there are finding ids
+    stratDescQuery += ",".join(["?"]*len(findings)) + ");"
+    for row in c.execute(stratDescQuery, findings):
+        if row[0] not in findingStratComps: # first time seeing this findingid, set up list
+            findingStratComps[row[0]] = list()
+        # now fill in this stratum component
+        findingStratComps[row[0]].append ({
+                "studyid": row[1],
+                "stratCompID": row[2],
+                "stratum": row[3],
+                "description":row[4],
+                "hint": row[5]})
+
+    # now reshape data so we have a dictionary of stratDesc tuples linked to findings
+    stratDescTuples = dict()
+    for findingid, fSC in findingStratComps.items():
+        thisFindingStratComps = list()
+        for sC in fSC:
+            thisFindingStratComps.append(sC["stratCompID"])
+        thisTuple = tuple(sorted(thisFindingStratComps))
+        if thisTuple not in stratDescTuples:
+            stratDescTuples[thisTuple] = {"tupleID": thisTuple, "studyids": set(), "freq":0, "findings": list(), "stratComps": list()}
+            for sC in fSC:
+                stratDescTuples[thisTuple]["stratComps"].append({
+                    "stratum": sC["stratum"],
+                    "description": sC["description"],
+                    "hint": sC["hint"]})
+        stratDescTuples[thisTuple]["freq"] += 1;
+        stratDescTuples[thisTuple]["findings"].append(findingid)
+        stratDescTuples[thisTuple]["studyids"].add(fSC[0]["studyid"])
+    
+    for tup in stratDescTuples:
+        stratDescTuples[tup]["numStudies"] = len(stratDescTuples[tup]["studyids"])
+        del stratDescTuples[tup]["studyids"]
+    stratDescTuplesByFreq = sorted(list(stratDescTuples.items()), key=lambda k_v:(k_v[1]["numStudies"], k_v[1]["freq"],k_v[0]), reverse=True)
+
+    # finally, send the whole shebang to the client
+    return json.dumps(stratDescTuplesByFreq)
+    
+
+@socketio.on('clientConnectionEvent')
+def socket_connect(message):
+    print(message)
+
+@socketio.on('stratCompRequest')
+def handle_stratCompRequest(message):
+    stratA = message[0]
+    stratB = message[1]
+    c = db.cursor()
+    findingsByStratQuery = """
+        select f.id from findings f join findingInstances fi on f.id = fi.findingid join stratumComponents sc on fi.stratcompid = sc.id where sc.stratum = ? 
+        INTERSECT
+        select f.id from findings f join findingInstances fi on f.id = fi.findingid join stratumComponents sc on fi.stratcompid = sc.id where sc.stratum = ?; 
+        """
+    findings = list();
+    findingStratComps = dict();
+    for row in c.execute(findingsByStratQuery, (stratA, stratB)):
+        findings.append(row[0])
+    # now build the query that fetches us strat descriptors for all the findings we've retrieved
+    stratDescQuery = """
+        select f.id, f.studyid, sc.id, sc.stratum, sc.description, sc.hint
+        from findings f 
+        join findingInstances fi on f.id = fi.findingid
+        join stratumComponents sc on fi.stratcompid = sc.id
+        WHERE f.id IN ("""
+    # add as many placeholders for the "WHERE f.id IN" clause as there are finding ids
+    stratDescQuery += ",".join(["?"]*len(findings)) + ");"
+    for row in c.execute(stratDescQuery, findings):
+        if row[0] not in findingStratComps: # first time seeing this findingid, set up list
+            findingStratComps[row[0]] = list()
+        # now fill in this stratum component
+        findingStratComps[row[0]].append ({
+                "studyid": row[1],
+                "stratCompID": row[2],
+                "stratum": row[3],
+                "description":row[4],
+                "hint": row[5]})
+
+    # now reshape data so we have a dictionary of stratDesc tuples linked to findings
+    stratDescTuples = dict()
+    for findingid, fSC in findingStratComps.items():
+        thisFindingStratComps = list()
+        for sC in fSC:
+            thisFindingStratComps.append(sC["stratCompID"])
+        thisTuple = tuple(sorted(thisFindingStratComps))
+        if thisTuple not in stratDescTuples:
+            stratDescTuples[thisTuple] = {"tupleID": thisTuple, "studyids": set(), "freq":0, "findings": list(), "stratComps": list()}
+            for sC in fSC:
+                stratDescTuples[thisTuple]["stratComps"].append({
+                    "stratum": sC["stratum"],
+                    "description": sC["description"],
+                    "hint": sC["hint"]})
+        stratDescTuples[thisTuple]["freq"] += 1;
+        stratDescTuples[thisTuple]["findings"].append(findingid)
+        stratDescTuples[thisTuple]["studyids"].add(fSC[0]["studyid"])
+    
+    for tup in stratDescTuples:
+        stratDescTuples[tup]["numStudies"] = len(stratDescTuples[tup]["studyids"])
+        del stratDescTuples[tup]["studyids"]
+    stratDescTuplesByFreq = sorted(list(stratDescTuples.items()), key=lambda k_v1:(k_v1[1]["numStudies"], k_v1[1]["freq"],k_v1[0]), reverse=True)
+
+    # finally, send the whole shebang to the client
+    emit('stratCompRequestHandled', stratDescTuplesByFreq)
+   
+                
+@socketio.on('showFindingsRequest')
+def handle_showFindingsRequest(message):
+    print(message)
+    tupleID = message["tupleID"]
+    findingIDs = message["findings"]
+    c = db.cursor()
+    findingsDetailsQuery = """
+        select f.id, f.description, s.id, s.description, s.methodology, s.sampleframe, s.samplesize, s.limitations, s.comments, s.reliability, a.title, a.authors, a.journal, a.year, a.id
+        from findings f 
+        join studies s on f.studyid = s.id
+        join articles a on s.articleid = a.id
+        WHERE f.id IN ("""
+    # add as many placeholders for the "WHERE f.id IN" clause as there are finding ids
+    findingsDetailsQuery += ",".join(["?"]*len(findingIDs)) + ");"
+    findings = list()
+    for row in c.execute(findingsDetailsQuery, findingIDs):
+        findings.append({ 
+            "findingid": row[0],
+            "f.description": row[1],
+            "s.id": row[2],
+            "s.description": row[3],
+            "s.methodology": row[4],
+            "s.samplesize": row[5],
+            "s.sampleframe": row[6],
+          # "s.limitations": row[7],
+          # "s.comments": row[8],
+          # "s.reliability": row[9],
+            "a.title": row[10],
+            "a.authors": row[11],
+            "a.journal": row[12],
+            "a.year": row[13],
+	    "a.id": row[14]})
+    emit("showFindingsRequestHandled", {
+        "tupleID": tupleID,
+        "findings": findings});
+
+        
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0")
+    app.debug = False 
